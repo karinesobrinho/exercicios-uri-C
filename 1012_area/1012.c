25fe2cedbfb7dd9a1c2f67014a00260cd4239efd
karine sobrinho <karine.silva.sobrinho@gmail.com>
2021-02-23 15:29:41 -0300

    1012

---
+++ 1012_area/1012.c
@@ -0,0 +1,19 @@
+//area
+
+#include <stdio.h>
+ 
+int main() {
+    double a, b, c, d;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    
+    double triangulo = (a*c)/2;
+    double circulo = 3.14159*(c*c);
+    double trapezio = ((a+b)*c)/2;
+    double quadrado = b*b;
+    double retangulo = a*b;
+    
+    printf("TRIANGULO: %.3lf\nCIRCULO: %.3lf\nTRAPEZIO: %.3lf\nQUADRADO: %.3lf\nRETANGULO: %.3lf\n", triangulo, circulo, trapezio, quadrado, retangulo);
+    
+ 
+    return 0;
+}
