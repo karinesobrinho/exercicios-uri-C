b910bea682ef40472dcad9834790e3696f15ec44
karine sobrinho <karine.silva.sobrinho@gmail.com>
2021-02-24 12:06:02 -0300

    1013

---
+++ 1013_o_maior/1013.c
@@ -0,0 +1,17 @@
+// O maior
+#include <stdio.h>
+ 
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if(a>=b && a>=c){
+        printf("%d eh o maior\n", a);
+    } else if(b>=a && b>=c){
+        printf("%d eh o maior\n", b);
+    } else if(c>=b && c>=a){
+        printf("%d eh o maior\n", c);
+    }
+ 
+    return 0;
+}
