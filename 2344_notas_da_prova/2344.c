69c4f8a383f693cd56fcf57305501d265911e28b
karine sobrinho <karine.silva.sobrinho@gmail.com>
2021-08-17 10:39:29 -0300

    2344

---
+++ 2344_notas_da_prova/2344.c
@@ -0,0 +1,23 @@
+//2344 - Notas da Prova
+//c99
+
+#include <stdio.h>
+
+int main(){
+    int x;
+    scanf("%d", &x);
+
+    if(x == 0 ){
+        printf("E\n");
+    } else if(x>= 1 && x<= 35){
+        printf("D\n");
+    } else if(x>= 36 && x<= 60){
+        printf("C\n");
+    } else if(x>= 61 && x<=85){
+        printf("B\n");
+    } else if(x>=86){
+        printf("A\n");
+    }
+    
+        return 0;
+}
