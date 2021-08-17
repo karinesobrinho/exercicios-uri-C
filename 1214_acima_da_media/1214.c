a0ba79785b4cfdc325c0fe96e15a8e8a89dfb7fd
karine sobrinho <karine.silva.sobrinho@gmail.com>
2021-08-17 10:39:11 -0300

    1214

---
+++ 1214_acima_da_media/1214.c
@@ -0,0 +1,25 @@
+///TODO arrumar
+
+#include <stdio.h>
+
+int main() {
+	int c;
+	scanf("%d", &c);
+
+	while(c!=0){
+		int n;
+		scanf("%d", &n);
+		while(n!=0){
+			scanf()
+
+			printf("% \n", porc);
+			n--;
+		}
+		c--;
+	}
+
+	
+    printf("Hello, world!");
+    return 0;
+}
+  
